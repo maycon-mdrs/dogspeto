eed335863a7a6c4b1ca1014f43a18e38e03550cf
Maycon Douglas <mdrs.maycon@gmail.com>
2023-03-13 00:24:52 -0300

    modal - all

--- cardapio/modal.js
+++ cardapio/modal.js
@@ -1,16 +1,32 @@
 /* ---------------------------------------------------------- */
 /* ------------------------ OPEN MODAL ---------------------- */
 /* ---------------------------------------------------------- */
+var open = false;
 function openModal() {
-    let modal = document.querySelector('.modal');
+    let modal = document.querySelector('.modal-content');
     modal.style.display = 'block';
+    console.log("abrir modal");
+    open = true;
 }
-
 
 /* ---------------------------------------------------------- */
 /* ----------------------- CLOSE MODAL ---------------------- */
 /* ---------------------------------------------------------- */
 function closeModal() {
-    let modal = document.querySelector('.modal');
+    let modal = document.querySelector('.modal-content');
     modal.style.display = 'none';
+    console.log("fehcar modal");
+    open = false;
 }
+
+/* MODAL - INSTRUÇÕES */
+function showModal() {
+    let modal = document.querySelector('.modal');
+    modal.classList.add("show-modal");
+}
+
+function hiddenModal() {
+    let modal = document.querySelector('.modal');
+    modal.classList.remove("show-modal");
+}
+
