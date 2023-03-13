2232816bb30d1d2eff80016a2457e1d3192a13f6
Maycon Douglas <mdrs.maycon@gmail.com>
2023-03-13 20:13:13 -0300

    <a>

--- cardapio/lista-produtos/combos.js
+++ cardapio/lista-produtos/combos.js
@@ -6,7 +6,7 @@ export const combos = [
          id: "combos",
          imgProduct: "/cardapio/produtos/combo.jpg",
          title: "COMBO",
-         description: "Qualquer Cachorro-Quente mais R$8,00 vira combo de Batata Frita - 300g, Regrigente e Molho (Maionese Verde, Barbecue, Puro Alho ou Ketchup).",
+         description: "Qualquer Cachorro-Quente mais R$8,00 vira combo de Batata Frita - 150g, Refrigente e Molho (Maionese Verde, Barbecue, Puro Alho, Rosé ou Ketchup).",
          valor: "Valor do Cachorro-Quente + R$8,00"
      },
  ];
